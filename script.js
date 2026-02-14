20dbf5b9b568bf6bbf35768523e3af7c8543f586
codeSystemWeb <codesystemweb@gmail.com>
2026-02-13 22:08:14 -0300

    SEO

--- script.js
+++ script.js
@@ -34,3 +34,29 @@ function typeWriter(elemento) {
   const titulo = document.querySelector("#movimento");
   typeWriter(titulo);
 
+document.getElementById("formContato").addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const nome = document.querySelector('[name="nome"]').value;
+    const whats = document.querySelector('[name="whats"]').value;
+    const mensagem = document.querySelector('[name="mensagem"]').value;
+    const honeypot = document.querySelector('[name="empresa"]').value;
+
+    // Anti-spam
+    if(honeypot){
+        return;
+    }
+
+    const numeroDestino = "5551997600239"; // SEU WHATS (com DDI + DDD)
+
+    const texto = `Olá, vim pelo site!\n\n` +
+                  `*Nome:* ${nome}\n` +
+                  `*WhatsApp:* ${whats}\n` +
+                  `*Mensagem:* ${mensagem}`;
+
+    const textoCodificado = encodeURIComponent(texto);
+
+    const url = `https://wa.me/${numeroDestino}?text=${textoCodificado}`;
+
+    window.open(url, "_blank");
+});
